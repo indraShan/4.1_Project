78cc3fc4d9230469bb784edd09ce02c2d5e730b3
indrajit <indrashanbhag@gmail.com>
2019-04-26 19:08:08 -0400

    Statistics first cut

--- Statistics.h
+++ Statistics.h
@@ -1,10 +1,30 @@
 #ifndef STATISTICS_
 #define STATISTICS_
 #include "ParseTree.h"
+#include <unordered_map>
+using namespace std;
+
+class Relation {
+public:
+	int numTuples;
+	int joinedRelationCount;
+	unordered_map<string, int> *attributes;
+
+	Relation();
+	friend std::ostream& operator<< (std::ostream& os, const Relation& relation);
+};
 
 
 class Statistics
 {
+private:
+	unordered_map<string, Relation*> *store;
+	// Can we do better than storing this value here?
+	double tempRes;
+	void printStore();
+	double Estimate(struct AndList *parseTree, char **relNames, int numToJoin, bool estimate);
+	int validateParams(struct AndList *parseTree, char **relNames, int numToJoin);
+
 public:
 	Statistics();
 	Statistics(Statistics &copyMe);	 // Performs deep copy
@@ -22,5 +42,6 @@ public:
 	double Estimate(struct AndList *parseTree, char **relNames, int numToJoin);
 
 };
+
 
 #endif
